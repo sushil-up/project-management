27882306911a68c55781a744f2a2ed672d6ded3b
Sushil <sushil.webexp@gmail.com>
2024-12-17 18:16:18 +0530

    add the provider

---
+++ src/app/admin-panel/addproject/page.jsx
@@ -0,0 +1,16 @@
+import { Container, Typography } from "@mui/material";
+import React from "react";
+
+const AddProject = () => {
+  return (
+    <>
+      <Container>
+        <Typography>
+        
+        </Typography>
+      </Container>
+    </>
+  );
+};
+
+export default AddProject;
---
+++ src/app/admin-panel/all-projects/page.js
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+const AllProjects = () => {
+  return (
+    <div>
+   <Typography className='text-center' variant='h4'>This is All Appointment</Typography>
+    </div>
+  )
+}
+
+export default AllProjects
