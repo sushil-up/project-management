9a9d3289836c480219fa70636f84f6128dd0bd38
Sushil <sushil.webexp@gmail.com>
2024-12-18 18:22:59 +0530

    change

--- src/utils/pagesurl.jsx
+++ src/utils/pagesurl.jsx
@@ -7,6 +7,6 @@ export const routesUrl = {
   test:'/admin-panel/test1',
   test2:'/admin-panel/testpage2',
   test0:'/admin-panel/test0',
-  task:'/admin-panel//task',
-  addProject:'/admin-panel//addproject'
+  task:'/admin-panel/task',
+  addProject:'/admin-panel/addproject'
 };
